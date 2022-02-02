288516615cd26b680bd78e318a29d30c644abe8a
ASHISH DARJI <ashdarji1@gmail.com>
2022-02-02 17:41:58 -0500

    mostly done, just testing needed and potentially add the bonus

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,18 +1,19 @@
 class ApiController < ApplicationController
+
   def ping
     respond_to do |format|
       format.json  { render :json => { "success": true, "response_status_code": 200 } }
     end
   end
+
   def posts
     require 'net/http'
+    require 'json'
+
+    @posts = []
 
     sortBy = ['id', 'reads', 'likes', 'popularity']
     direction = ['asc', 'desc']
-
-    if params['tag']
-      tags = params['tag'].strip
-    end
 
     if params['sortby']
       if sortBy.include?(params['sortby'])
@@ -37,15 +38,25 @@ class ApiController < ApplicationController
       direction = direction[0]
     end
 
-    api_endpoint = URI("https://api.hatchways.io/assessment/blog/posts?tag=#{tags}&sortby=#{sortBy}&direction=#{direction}")
+    if params['tag']
+      tags = params['tag'].strip
 
-    @posts = Net::HTTP.get(api_endpoint)
+      tags.split(",").each do |tag|
+        @posts = @posts.flatten
+        api_endpoint = URI("https://api.hatchways.io/assessment/blog/posts?tag=#{tag}&sortby=#{sortBy}&direction=#{direction}")
+        results = JSON.parse(Net::HTTP.get(api_endpoint))["posts"]
+        @posts.push(results)
+      end
+    end
 
-    puts api_endpoint
-    puts @posts
+    @posts = {
+      "posts_count": @posts.count,
+      "posts": @posts
+    }
 
     respond_to do |format|
       format.json  { render :json => @posts }
     end
+
   end
 end
