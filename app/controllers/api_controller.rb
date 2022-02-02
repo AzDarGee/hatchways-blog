4a0d14fb0849406c707c6784db8a36cf9c38e25e
ASHISH DARJI <ashdarji1@gmail.com>
2022-02-02 17:12:39 -0500

    progress on the posts api action

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,8 +5,44 @@ class ApiController < ApplicationController
     end
   end
   def posts
+    require 'net/http'
 
-    @posts = 
+    sortBy = ['id', 'reads', 'likes', 'popularity']
+    direction = ['asc', 'desc']
+
+    if params['tag']
+      tags = params['tag'].strip
+    end
+
+    if params['sortby']
+      if sortBy.include?(params['sortby'])
+        sortBy = params['sortby']
+      else
+        # Default value is 'id'
+        sortBy = sortBy[0]
+      end
+
+    else
+      sortBy = sortBy[0]
+    end
+
+    if params['direction']
+      if direction.include?(params['direction'])
+        direction = params['direction']
+      else
+        # Default is 'asc'
+        direction = direction[0]
+      end
+    else
+      direction = direction[0]
+    end
+
+    api_endpoint = URI("https://api.hatchways.io/assessment/blog/posts?tag=#{tags}&sortby=#{sortBy}&direction=#{direction}")
+
+    @posts = Net::HTTP.get(api_endpoint)
+
+    puts api_endpoint
+    puts @posts
 
     respond_to do |format|
       format.json  { render :json => @posts }
