f5ae143ef1b515370359f2ac18f684f84c34795d
ASHISH DARJI <ashdarji1@gmail.com>
2022-02-02 19:36:09 -0500

    sorting and error handling

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -11,6 +11,7 @@ class ApiController < ApplicationController
     require 'json'
 
     @posts = []
+    @error_msg = []
 
     sortBy = ['id', 'reads', 'likes', 'popularity']
     direction = ['asc', 'desc']
@@ -21,10 +22,11 @@ class ApiController < ApplicationController
       else
         # Default value is 'id'
         sortBy = sortBy[0]
+        @error_msg << "SortBy parameter is invalid"
       end
-
     else
       sortBy = sortBy[0]
+      @error_msg << "SortBy parameter is required"
     end
 
     if params['direction']
@@ -33,8 +35,10 @@ class ApiController < ApplicationController
       else
         # Default is 'asc'
         direction = direction[0]
+        @error_msg << "Direction parameter is invalid"
       end
     else
+      @error_msg << "Direction parameter is required"
       direction = direction[0]
     end
 
@@ -47,23 +51,64 @@ class ApiController < ApplicationController
         results = JSON.parse(Net::HTTP.get(api_endpoint))["posts"]
         @posts.push(results)
       end
+
+      # Flatten all posts
+      @posts = @posts.flatten
+      # Only unique objects in @posts
+      @posts = @posts.uniq { |p| p["id"] }
+    else
+      @error_msg << "Tag parameter is required"
     end
 
-    # if direction == 'asc'
-    #   sorted_array = @posts.sort_by { |hash| hash['id'].to_i }
-    # else
-    #   sorted_array = @posts.sort_by { |hash| hash['id'].to_i }.reverse
-    # end
-    #
-    puts @posts
+    # Sort the array
+    if direction == 'asc'
+      case sortBy
+      when "reads"
+        sorted_array = @posts.sort_by{ |e| e['reads'].to_i }
+      when "likes"
+        sorted_array = @posts.sort_by{ |e| e['likes'].to_i }
+      when "popularity"
+        sorted_array = @posts.sort_by{ |e| e['popularity'].to_i }
+      when "id"
+        sorted_array = @posts.sort_by{ |e| e['id'].to_i }
+      else
+        sorted_array = @posts.sort_by{ |e| e['id'].to_i }
+      end
+
+    elsif direction == 'desc'
+      case sortBy
+      when "reads"
+        sorted_array = @posts.sort_by{ |e| e['reads'].to_i }.reverse
+      when "likes"
+        sorted_array = @posts.sort_by{ |e| e['likes'].to_i }.reverse
+      when "popularity"
+        sorted_array = @posts.sort_by{ |e| e['popularity'].to_i }.reverse
+      when "id"
+        sorted_array = @posts.sort_by{ |e| e['id'].to_i }.reverse
+      else
+        sorted_array = @posts.sort_by{ |e| e['id'].to_i }.reverse
+      end
+
+    else
+      @error_msg = ""
+    end
 
     @posts = {
-      "posts_count": @posts.count,
-      "posts": @posts
+      "posts_count": sorted_array.count,
+      "posts": sorted_array
     }
 
     respond_to do |format|
-      format.json  { render :json => @posts }
+      if not params['tag'].nil?
+        format.json  { render :json => @posts }
+      else
+        format.json {
+          render :json => {
+            "error": @error_msg,
+            "response_status_code": 400
+          }
+        }
+      end
     end
 
   end
